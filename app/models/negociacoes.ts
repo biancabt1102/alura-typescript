f22aff64c8ada4e3cc36744c5b0d5123d3d0ba55
Bianca Teixeira <biancabt1102@gmail.com>
2023-07-18 15:48:15 -0300

    Avançando na linguagem

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -4,12 +4,12 @@ export class Negociacoes {
     // ou Array<Negociacao>
     private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao ){
+    public adiciona(negociacao: Negociacao ){
         this.negociacoes.push(negociacao);
     }
 
     // ou ReadonlyArray<Negociacao>
-    lista(): readonly Negociacao[] {
+    public lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,12 +1,10 @@
 export class Negociacoes {
     constructor() {
-        // ou Array<Negociacao>
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    // ou ReadonlyArray<Negociacao>
     lista() {
         return this.negociacoes;
     }
